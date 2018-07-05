ad29f3e007e247b9fa9129e33e6c39bdfbe7536d
lee61011 <lee61011@126.com>
2018-07-05 18:34:00 +0800

    Vue + Koa2 在线商城项目

--- SmileVue/service/database/schema/User.js
+++ SmileVue/service/database/schema/User.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')    //引入Mongoose
 const Schema = mongoose.Schema          //声明Schema
 let ObjectId = Schema.Types.ObjectId    //声明Object类型
 
+const bcrypt = require( 'bcrype' )
+
 //创建我们的用户Schema
 const userSchema = new Schema({
   UserId:ObjectId,
@@ -10,6 +12,20 @@ const userSchema = new Schema({
   createAt:{type:Date,default:Date.now()},
   lastLoginAt:{type:Date,default:Date.now()}
 
+})
+
+//每次存储数据时都要执行
+userSchema.pre('save', function(next){
+  //	let user = this
+  //	console.log(this)
+  bcrypt.genSalt( SALT_WORK_FACTOR,(err,salt)=>{
+    if(err) return next(err)
+    bcrypt.hash(this.password,salt, (err,hash)=>{
+      if(err) return next(err)
+      this.password = hash
+      next()
+    })
+  })
 })
 
 //发布模型
