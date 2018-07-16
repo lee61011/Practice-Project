b338deac13bb31a02f21328b383c30bf05d42c9b
lee61011 <lee61011@126.com>
2018-07-16 16:09:28 +0800

    批量插入商品详情数据到 MongoDB 中

---
+++ SmileVue/service/appApi/goods.js
@@ -0,0 +1,28 @@
+const Koa = require('koa');
+const app = new Koa();
+const Router = require('koa-router');
+let router = new Router();
+
+const mongoose = require('mongoose');
+const fs = require('fs');
+
+router.get('/insertAllGoodsInfo',async(ctx)=>{
+  fs.readFile('./goods.json','utf8',(err,data)=>{
+    data = JSON.parse(data);
+    let saveCount = 0;
+    const Goods = mongoose.model('Goods');
+    data.map((value,index)=>{
+      console.log(value);
+      let newGoods = new Goods(value);
+      newGoods.save().then(()=>{
+        saveCount++;
+        console.log('成功' + saveCount)
+      }).catch(error => {
+        console.log('失败：' + error)
+      })
+    })
+  })
+  ctx.body = "开始导入数据!"
+})
+
+module.exports = router;
