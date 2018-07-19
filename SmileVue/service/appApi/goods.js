1f7b9ab30e2c3d7589e0fcabf72f202913ce0a33
lee61011 <lee61011@126.com>
2018-07-19 18:45:09 +0800

    分类页面的数据读取

--- SmileVue/service/appApi/goods.js
+++ SmileVue/service/appApi/goods.js
@@ -77,4 +77,38 @@ router.post('/getDetailGoodsInfo',async(ctx)=>{
     })
 });
 
+router.get('/getCategoryList',async(ctx) => {
+  try{
+    const Category = mongoose.model('Category');
+    let result = await Category.find().exec();
+    ctx.body = {code:200, message:result};
+  } catch (err) {
+    ctx.body = {code: 500, message: err}
+  }
+});
+
+router.get('/getCategorySubList',async(ctx) => {
+  try{
+    let categoryId = ctx.request.body.categoryId;
+    //  let categoryId = 1;
+    const CategorySub = mongoose.model('CategorySub');
+    let result = await CategorySub.find({MALL_CATEGORY_ID:categoryId}).exec();
+    ctx.body = {code:200, message:result}
+  } catch(err) {
+    ctx.body = {code:500, message:err}
+  }
+});
+
+router.get('/getGoodsListByCategorySubID',async(ctx) => {
+  try{
+    //	let categorySubId = ctx.request.body.categoryId
+    let categorySubId = '2c9f6c946016ea9b016016f79c8e0000';
+    const Goods = mongoose.model('Goods');
+    let result = await Goods.find({SUB_ID:categorySubId}).exec();
+    ctx.body = {code:200, message: result};
+  } catch(err) {
+    ctx.body = {code:500, message:err}
+  }
+});
+
 module.exports = router;
