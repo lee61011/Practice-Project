030544c6438a77cacf44665af4cfdbc3b68a57bd
lee61011 <lee61011@126.com>
2018-07-20 13:46:14 +0800

    商品列表页的大类交互效果

--- SmileVue/src/components/pages/CategoryList.vue
+++ SmileVue/src/components/pages/CategoryList.vue
@@ -8,7 +8,11 @@
       <van-row>
         <van-col span="6">
           <div id="leftNav">
-            左侧导航
+            <ul>
+              <li @click="clickCategory(index)" :class="{categoryActive:categoryIndex==index}" v-for="(item,index) in category" :key="index">
+                {{item.MALL_CATEGORY_NAME}}
+              </li>
+            </ul>
           </div>
         </van-col>
 
@@ -27,7 +31,8 @@
   export default {
     data(){
       return {
-
+        category: [],
+        categoryIndex:0,
       }
     },
 
@@ -35,31 +40,49 @@
       this.getCategory();
     },
 
+    mounted(){
+      let winHeight = document.documentElement.clientHeight;
+      document.getElementById("leftNav").style.height = winHeight - 46 + "px";
+    },
+
     methods: {
       getCategory() {
         axios({
-          url:url.getCategoryList,
-          method:'get',
-        })
-          .then(response=>{
-
+          url: url.getCategoryList,
+          method: 'get',
+        }).then(response => {
           console.log(response)
+          if (response.data.code == 200 && response.data.message) {
+            this.category = response.data.message;
+          } else {
+            Toast('服务器错误，数据取得失败')
+          }
 
-        if(response.data.code == 200 && response.data.message ){
-
-        }else{
-          Toast('服务器错误，数据取得失败')
-        }
-
-      })
-      .catch(error=>{
+        }).catch(error => {
           console.log(error)
-      })
+        })
+      },
+
+      //点击大类的方法
+      clickCategory(index){
+        this.categoryIndex=index
       }
     },
   }
 </script>
 
 <style scoped>
-
+  #leftNav{
+    background-color: aliceblue;
+  }
+  #leftNav ul li {
+    line-height: 2rem;
+    border-bottom:1px solid #E4E7ED;
+    padding:3px;
+    font-size:0.8rem;
+    text-align: center;
+  }
+  .categoryActive{
+    background-color: #fff;
+  }
 </style>
