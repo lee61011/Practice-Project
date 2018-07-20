e49ab1563f52d65e8a3db671e700afa70b9f7373
lee61011 <lee61011@126.com>
2018-07-20 11:23:48 +0800

    商品列表页的布局1

---
+++ SmileVue/src/components/pages/CategoryList.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div class="navbar-div">
+      <van-nav-bar   title="类别列表" />
+    </div>
+
+    <div>
+      <van-row>
+        <van-col span="6">
+          <div id="leftNav">
+            左侧导航
+          </div>
+        </van-col>
+
+        <van-col span="18">
+          右侧列表
+        </van-col>
+      </van-row>
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  import url from '@/serviceAPI.config.js'
+
+  export default {
+    data(){
+      return {
+
+      }
+    },
+
+    created(){
+      this.getCategory();
+    },
+
+    methods: {
+      getCategory() {
+        axios({
+          url:url.getCategoryList,
+          method:'get',
+        })
+          .then(response=>{
+
+          console.log(response)
+
+        if(response.data.code == 200 && response.data.message ){
+
+        }else{
+          Toast('服务器错误，数据取得失败')
+        }
+
+      })
+      .catch(error=>{
+          console.log(error)
+      })
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
--- SmileVue/src/serviceAPI.config.js
+++ SmileVue/src/serviceAPI.config.js
@@ -4,8 +4,10 @@ const LOCALURL = "http://localhost:3000/"
 const URL = {
   getShoppingMallInfo:BASEURL+'index',
   getGoodsInfo:BASEURL+'getGoodsInfo',
-  registerUser:LOCALURL+'user/register',   //用户注册接口
-  login:LOCALURL+'user/login',   //用户注册接口
+  registerUser:LOCALURL+'user/register',                    //用户注册接口
+  login:LOCALURL+'user/login',                              //用户注册接口
+  getDetailGoodsInfo:LOCALURL+'goods/getDetailGoodsInfo',   //得到商品详细数据
+  getCategoryList:LOCALURL+'goods/getCategoryList',         //得到大类信息
 }
 
 module.exports = URL
