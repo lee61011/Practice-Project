7951b20ca1cd5faa4665f76807d4275be630e129
lee61011 <lee61011@126.com>
2018-10-11 18:05:41 +0800

    随机算法及实现(优化)

--- 数独游戏/src/js/index.js
+++ 数独游戏/src/js/index.js
@@ -1,36 +1,8 @@
-function makeRow(v = 0){
-    const array = new Array(9);
-    array.fill(v);
-    return array;
-}
+const toolkit = require("./toolkit")
 
-function makeMatrix(v = 0) {
-    /*  这种做法有问题
-        const array = new Array(9);
-        array.fill(makeRow(v));
-        return array;
-    */
-
-    /*return Array.from({length: 9})
-        .map(() => makeRow(v));*/
-
-    //  相对于上面的代码提高效率
-    return Array.from({length: 9}, () => makeRow(v));
-}
-
-
-/**
-*   Fisher-Yates    洗牌算法
-* */
-function shuffle(array) {
-    const endIndex = array.length - 2;
-    for (let i = 0; i < endIndex; i++) {
-        const j = i + Math.floor(Math.random() * (array.length - i));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-}
+const matrix = toolkit.makeMatrix();
+console.log(matrix);
 
 const a = Array.from({length: 9}, (v, i) => i);
 console.log(a);
-console.log(shuffle(a))
+console.log(toolkit.shuffle(a))
---
+++ 数独游戏/src/js/toolkit.js
@@ -0,0 +1,26 @@
+const matrixToolkit = {
+    makeRow(v = 0){
+        const array = new Array(9);
+        array.fill(v);
+        return array;
+    },
+
+    makeMatrix(v = 0) {
+        return Array.from({length: 9}, () => this.makeRow(v));
+    },
+
+
+    /**
+     *   Fisher-Yates    洗牌算法
+     * */
+    shuffle(array) {
+        const endIndex = array.length - 2;
+        for (let i = 0; i < endIndex; i++) {
+            const j = i + Math.floor(Math.random() * (array.length - i));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+};
+
+module.exports = matrixToolkit;
