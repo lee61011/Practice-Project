901dfc7164fdb3d226505373b40ea21ccad69bd2
lee61011 <lee61011@126.com>
2018-10-11 17:54:19 +0800

    随机算法及实现

--- 数独游戏/src/js/index.js
+++ 数独游戏/src/js/index.js
@@ -18,6 +18,19 @@ function makeMatrix(v = 0) {
     return Array.from({length: 9}, () => makeRow(v));
 }
 
-const a = makeMatrix();
-a[0][1] = 2;
+
+/**
+*   Fisher-Yates    洗牌算法
+* */
+function shuffle(array) {
+    const endIndex = array.length - 2;
+    for (let i = 0; i < endIndex; i++) {
+        const j = i + Math.floor(Math.random() * (array.length - i));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+const a = Array.from({length: 9}, (v, i) => i);
 console.log(a);
+console.log(shuffle(a))
