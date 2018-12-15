2addf184968c0afe07b989bcb687e520a3ef4452
lee61011 <lee61011@126.com>
2018-12-15 22:40:46 +0800

    主题日报

--- 知乎日报项目开发/daily/app.vue
+++ 知乎日报项目开发/daily/app.vue
@@ -1,21 +1,34 @@
 <template>
     <div class="daily">
         <div class="daily-menu">
-            <div class="daily-menu-item">每日推荐</div>
-            <div class="daily-menu-item">主题日报</div>
+            <div class="daily-menu-item"
+                :class="{ on: type === 'recommend' }">每日推荐</div>
+            <div class="daily-menu-item"
+                :class="{ on: type === 'daily'}"
+                @click="showThemes = !showThemes">主题日报</div>
+            <ul v-show="showThemes">
+                <li v-for="item in themes">
+                    <a :class="{ on: item.id === themeId && type === 'daily' }">{{ item.name }}</a>
+                </li>    
+            </ul>
         </div>
         <div class="daily-list">
-            <Item></Item>
+            <!-- <Item></Item> -->
         </div>
-        <daily-article></daily-article>
+        <!-- <daily-article></daily-article> -->
     </div>
 </template>
 
 <script>
     export default {
         data () {
-            return {}
-        }
+            return {
+                themes: [],
+                showThemes: false,
+                type: 'recommend',
+                themeId: 0
+            }
+        },
     }
 </script>
 
