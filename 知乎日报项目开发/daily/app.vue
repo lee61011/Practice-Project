32b4043b577f3a540baba3a5f1dfc4339ab42eba
lee61011 <lee61011@126.com>
2018-12-15 22:55:16 +0800

    主题日报

--- 知乎日报项目开发/daily/app.vue
+++ 知乎日报项目开发/daily/app.vue
@@ -8,7 +8,8 @@
                 @click="showThemes = !showThemes">主题日报</div>
             <ul v-show="showThemes">
                 <li v-for="item in themes">
-                    <a :class="{ on: item.id === themeId && type === 'daily' }">{{ item.name }}</a>
+                    <a :class="{ on: item.id === themeId && type === 'daily' }"
+                        @click="handleToTheme(item.id)">{{ item.name }}</a>
                 </li>    
             </ul>
         </div>
@@ -20,15 +21,40 @@
 </template>
 
 <script>
+    import $ from './libs/util'
     export default {
         data () {
             return {
                 themes: [],
                 showThemes: false,
                 type: 'recommend',
+                this: [],
                 themeId: 0
             }
         },
+        methods: {
+            getThemes () {
+                //  axios 发起请求
+                $.ajax.get('theme').then(res => {
+                    this.themes = res.others;
+                })
+            },
+            handleToTheme (id) {
+                //  改变菜单分类
+                this.type = 'daily';
+                //  设置当前点击子类的主题日报 id 
+                this.themeId = id;
+                //  清空中间栏的数据
+                this.list = [];
+                $.ajax.get('theme/' + id).then(res => {
+                    this.list = res.stories.filter(item => item.type !== 1);
+                })
+            }
+        },
+        mounted () {
+            //  初始化时调用
+            this.getThemes();
+        }
     }
 </script>
 
