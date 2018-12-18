93b86dae2dec20f7146b8896e4be25938bbf4cf3
lee61011 <lee61011@126.com>
2018-12-18 18:40:42 +0800

    每日推荐

--- 知乎日报项目开发/daily/app.vue
+++ 知乎日报项目开发/daily/app.vue
@@ -2,6 +2,7 @@
     <div class="daily">
         <div class="daily-menu">
             <div class="daily-menu-item"
+                 @click="handleToRecommend"
                 :class="{ on: type === 'recommend' }">每日推荐</div>
             <div class="daily-menu-item"
                 :class="{ on: type === 'daily'}"
@@ -21,39 +22,62 @@
 </template>
 
 <script>
-    import $ from './libs/util'
+    import $ from './libs/util';
+    import axios from 'axios';
     export default {
         data () {
             return {
                 themes: [],
                 showThemes: false,
                 type: 'recommend',
-                this: [],
-                themeId: 0
+                list: [],
+                themeId: 0,
+                recommendList: [],
+                dailyTime: $.getTodayTime(),
+                isLoading: false
             }
         },
         methods: {
             getThemes () {
-                //  axios 发起请求
-                $.ajax.get('theme').then(res => {
+                console.log('准备发请求')
+
+                $.ajax.get('themes').then(res => {
+                    //  cosnole.log('themes axios 发起get请求');
+                    console.log(res)
                     this.themes = res.others;
                 })
             },
             handleToTheme (id) {
                 //  改变菜单分类
                 this.type = 'daily';
-                //  设置当前点击子类的主题日报 id 
+                //  设置当前点击子类的主题日报 id
                 this.themeId = id;
-                //  清空中间栏的数据
+                //  清空中间栏数据
                 this.list = [];
                 $.ajax.get('theme/' + id).then(res => {
+                    //  过滤掉类型为 1 的文章, 该类型下面的文章为空
                     this.list = res.stories.filter(item => item.type !== 1);
+                })
+            },
+            handleToRecommend () {
+                this.type = 'recommend';
+                this.recommendList = [];
+                this.dailyTime = $.getTodayTime();
+                this.getRecommendList();
+            },
+            getRecommendList () {
+                this.isLoading = true;
+                const prevDay = $.prevDay(this.dailyTime + 86400000);
+                $.ajax.get('news/before/' + prevDay).then(res => {
+                    this.recommendList.push(res);
+                    this.isLoading = false;
                 })
             }
         },
         mounted () {
             //  初始化时调用
             this.getThemes();
+            this.getRecommendList();
         }
     }
 </script>
--- 知乎日报项目开发/daily/libs/util.js
+++ 知乎日报项目开发/daily/libs/util.js
@@ -14,7 +14,27 @@ Util.ajax = axios.create({
 });
 //  添加响应拦截器
 Util.ajax.interceptors.response.use(res => {
-    return res.data;
+    return res
+    //  return res.data;
 });
+
+
+//  获取今天的时间戳
+Util.getTodayTime = function () {
+    const date = new Date();
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setSeconds(0);
+    date.setMilliseconds(0);
+    return date.getTime();
+};
+//  获取前一天的日期
+Util.prevDay = function (timestamp = (new Date()).getTime()) {
+    const date = new Date(timestamp);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
+    const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+    return year + '' + month + '' + day
+};
 
 export default Util;
