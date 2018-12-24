7212e797efddd0037862bbf10d5df6e6de1dcea9
lee61011 <lee61011@126.com>
2018-12-24 22:23:40 +0800

    加载评论

--- 知乎日报项目开发/daily/components/daily-article.vue
+++ 知乎日报项目开发/daily/components/daily-article.vue
@@ -2,12 +2,28 @@
     <div class="daily-article">
         <div class="daily-article-title">{{ data.title }}</div>
         <div class="daily-article-content" v-html="data.body"></div>
+
+        <div class="daily-comments" v-show="comments.length">
+            <span>评论 ({{ comments.length }})</span>
+            <div class="daily-comment" v-for="comment in comments">
+                <div class="daily-comment-avatar">
+                    <img :src="comment.avatar">
+                </div>
+                <div class="daily-comment-content">
+                    <div class="daily-comment-name">{{ comment.author }}</div>
+                    <div class="daily-comment-time" v-time="comment.time"></div>
+                    <div class="daily-comment-text">{{ comment.content }}</div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    import $ from '../libs/util'
+    import Time from '../directives/time';
+    import $ from '../libs/util';
     export default {
+        directives: { Time },
         props: {
             id: {
                 type: Number,
@@ -16,7 +32,8 @@
         },
         data () {
             return {
-                data: {}
+                data: {},
+                comments: []
             }
         },
         methods: {
@@ -27,8 +44,19 @@
                     res.body = res.body.replace(/src="https/g, 'src="' + $imgPath + 'https');
                     this.data = res;
                     //  返回文章顶端
-                    window.scrollTo(0,0)
+                    window.scrollTo(0,0);
+                    this.getComments();
                 })
+            },
+            getComments () {
+                this.comments = [];
+                $.ajax.get('story' + this.id + '/short-comments').then(res => {
+                    this.comments = res.comments.map(comment => {
+                        //  将头像的图片地址转换为代理地址
+                        comment.avatar = $.imgPath + comment.avatar;
+                        return comment;
+                    });
+                });
             }
         },
         watch: {
