106df597d29e51795e39ca096b7e2c5670f38206
lee61011 <lee61011@126.com>
2019-01-16 21:45:41 +0800

    组件切换时的动画效果

--- Vue-cms/src/App.vue
+++ Vue-cms/src/App.vue
@@ -6,7 +6,9 @@
 
 
     <!-- 中间路由 router-view 区域 -->
-    <router-view></router-view>
+    <transition>
+      <router-view></router-view>
+    </transition>
 
     <!-- tabbar -->
     <nav class="mui-bar mui-bar-tab">
@@ -45,5 +47,21 @@ export default {
 <style lang="scss" scoped>
   .app-container {
     padding-top: 40px;
+    overflow-x: hidden;
+  }
+
+  .v-enter {
+    opacity: 0;
+    transform: translateX(100%);
+  }
+  .v-leave-to {
+    opacity: 0;
+    transform: translateX(-100%);
+    position: absolute;
+  }
+
+  .v-enter-active,
+  .v-leave-active {
+    transition: all 0.5s ease;
   }
 </style>
