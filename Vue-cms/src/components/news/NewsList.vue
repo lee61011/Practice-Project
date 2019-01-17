0e2410561e39bdc08f32590902a77104191cdec6
lee61011 <lee61011@126.com>
2019-01-17 14:32:01 +0800

    新闻资讯路由链接及页面布局

---
+++ Vue-cms/src/components/news/NewsList.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <ul class="mui-table-view">
+				<li class="mui-table-view-cell mui-media">
+					<a href="javascript:;">
+						<img class="mui-media-object mui-pull-left" src="https://img.souche.com/20161230/png/bc836261fbb654dda6b653e428014279.png">
+						<div class="mui-media-body">
+							<h1>幸福</h1>
+							<p class='mui-ellipsis'>
+                                <span>时间: 2019.01.11</span>
+                                <span>点击数量: 0 次</span>
+                            </p>
+						</div>
+					</a>
+				</li>
+				<li class="mui-table-view-cell mui-media">
+					<a href="javascript:;">
+						<img class="mui-media-object mui-pull-left" src="https://img.souche.com/20161230/png/bc836261fbb654dda6b653e428014279.png">
+						<div class="mui-media-body">
+							<h1>幸福</h1>
+							<p class='mui-ellipsis'>
+                                <span>时间: 2019.01.11</span>
+                                <span>点击数量: 0 次</span>
+                            </p>
+						</div>
+					</a>
+				</li>
+                <li class="mui-table-view-cell mui-media">
+					<a href="javascript:;">
+						<img class="mui-media-object mui-pull-left" src="https://img.souche.com/20161230/png/bc836261fbb654dda6b653e428014279.png">
+						<div class="mui-media-body">
+							<h1>幸福</h1>
+							<p class='mui-ellipsis'>
+                                <span>时间: 2019.01.11</span>
+                                <span>点击数量: 0 次</span>
+                            </p>
+						</div>
+					</a>
+				</li>
+			</ul>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.mui-table-view {
+
+    h1 {
+        font-size: 14px;
+    }
+    .mui-ellipsis {
+        font-size: 12px;
+        color: #226aff;
+        display: flex;
+        justify-content: space-between;
+    }
+}
+</style>
--- Vue-cms/src/router.js
+++ Vue-cms/src/router.js
@@ -5,15 +5,18 @@ import HomeContainer from './components/tabbar/HomeContainer.vue';
 import MemberContainer from './components/tabbar/MemberContainer.vue';
 import ShopcarContainer from './components/tabbar/ShopcarContainer.vue';
 import SearchContainer from './components/tabbar/SearchContainer.vue';
+import NewsList from './components/news/NewsList.vue'
 
 // 3. 创建路由对象
 var router = new VueRouter({
   routes: [ // 配置路由规则
-    { path: '/', redirect: '/home' },
+    { path: '/', redirect: '/home' },         //  如果是根路径的话, 将路由重定向到 home 组件
     { path: '/home', component: HomeContainer },
     { path: '/member', component: MemberContainer },
     { path: '/shopcar', component: ShopcarContainer },
     { path: '/search', component: SearchContainer },
+
+    { path: '/home/newsList', component: NewsList },
   ],
   linkActiveClass: 'mui-active' //  当路由选中时, 覆盖默认的路由高亮的类, 默认是 router-link-active
 })
