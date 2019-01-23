c21477c6d2ee60219e1364410b3823a86dd89feb
lee61011 <lee61011@126.com>
2019-01-23 23:37:02 +0800

    加入购物车动画, 修改小球位置偏移问题

--- Vue-cms/src/components/goods/GoodsInfo.vue
+++ Vue-cms/src/components/goods/GoodsInfo.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="goodsinfo-content">
+
+        <transition
+            @before-enter="beforeEnter"
+            @enter="enter"
+            @after-enter="afterEnter">
+            <div id="boll" v-show="bollFlag" ref="boll"></div>
+        </transition>
         
         <!-- 商品轮播图区域 -->
         <div class="mui-card">
@@ -19,7 +26,7 @@
                         <p>购买数量：<numbox></numbox></p>
                         <p>
                             <mt-button type="primary" size="small">立即购买</mt-button>
-                            <mt-button type="danger" size="small">加入购物车</mt-button>
+                            <mt-button type="danger" size="small" @click="addToShopCar">加入购物车</mt-button>
                             
                         </p>
 					</div>
@@ -55,7 +62,8 @@ export default {
         return {
             id: this.$route.params.id,
             lunbotu: [],
-            goodsinfo: {}
+            goodsinfo: {},
+            bollFlag: false     //  控制加入购物车小球的显示与隐藏, 默认隐藏状态
         }
     },
     created() {
@@ -79,6 +87,32 @@ export default {
         },
         goCommon (id) {
             this.$router.push({ name: "goodscommon", params: { id } })
+        },
+        addToShopCar() {
+            this.bollFlag = !this.bollFlag;
+        },
+        beforeEnter (el) {
+            el.style.transform = 'translate(0, 0)';
+        },
+        enter (el, done) {
+            el.offsetWidth;
+
+            //  console.log( this.$refs.boll )
+
+            //  获取小球在页面中的位置
+            const bollPosition = this.$refs.boll.getBoundingClientRect();
+            //  获取徽章在页面中的位置, 使用 DOM 操作的方法
+            const badgePosition = document.getElementById('badge').getBoundingClientRect();
+
+            const xDist = badgePosition.left - bollPosition.left;
+            const yDist = badgePosition.top - bollPosition.top;
+
+            el.style.transform = `translate(${xDist}px, ${yDist}px)`;
+            el.style.transition = "all 1s ease"
+            done();
+        },
+        afterEnter (el) {
+            this.bollFlag = !this.bollFlag;
         },
     },
     components: {
@@ -105,6 +139,18 @@ export default {
         button {
             margin: 15px 0;
         }
+    }
+
+    #boll {
+        width: 15px;
+        height: 15px;
+        border-radius: 50%;
+        background-color: red;
+
+        position: absolute;
+        left: 146px;
+        top: 390px;
+        z-index: 999;
     }
 }
 </style>
